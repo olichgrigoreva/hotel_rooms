a213f791549ff565a00a868693531dafd9a1c1d2
Olga Grigoreva <aboutrocky94@gmail.com>
2019-09-10 20:55:57 +0300

    верстка по бэм, js

--- js/main.js
+++ js/main.js
@@ -2,21 +2,15 @@
  * Created by Olya on 02.09.2019.
  */
 document.addEventListener("DOMContentLoaded", function () {
-
-        let room_card = document.querySelectorAll(".room_card");
-
-        for (let i = 0; i < room_card.length; i++) {
-            let btn = room_card[i].querySelector(".room_order_btn");
-            btn.addEventListener('click', function () {
-                room_card[i].addEventListener('mouseleave', function _listener () {
-                    room_card[i].classList.toggle("grayscale");
-                    if (room_card[i].classList.contains('grayscale')) {
-                        btn.value = "Подробнее";
-                    }
-                    else {
-                        btn.value = "Забронировать";
-                    }
-                    room_card[i].removeEventListener("mouseleave", _listener);
+        let card = document.querySelectorAll(".card");
+        for (let i = 0; i < card.length; i++) {
+            let btn = card[i].querySelector(".button");
+            btn.addEventListener('click', function (event) {
+                event.preventDefault();
+                card[i].addEventListener('mouseleave', function _listener() {
+                    card[i].classList.toggle("grayscale");
+                    btn.textContent = btn.textContent === 'Подробнее' ? 'Забронировать' : 'Подробнее';
+                    card[i].removeEventListener("mouseleave", _listener);
                 });
             });
         }
